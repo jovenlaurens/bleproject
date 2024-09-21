b2a94cf1a17af1daba24512bfa540241b97c3645
jovenlaurens <joven.nicholas@gmail.com>
2024-09-21 18:19:13 +0700

    Removed ip restriction

--- app/src/main/java/com/example/bletutorial/api/ApiInstance.kt
+++ app/src/main/java/com/example/bletutorial/api/ApiInstance.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 val retrofit = Retrofit.Builder()
-    .baseUrl("http://127.0.0.1:5000")
+    .baseUrl("http://192.168.31.84:5000")
     .addConverterFactory(GsonConverterFactory.create())
     .build()
 
