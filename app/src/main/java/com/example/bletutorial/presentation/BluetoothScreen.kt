d9e55ccd6b3c91e5720e9a02979f807678cf1d64
jovenlaurens <joven.nicholas@gmail.com>
2024-09-22 02:36:00 +0700

    fixed data size

--- app/src/main/java/com/example/bletutorial/presentation/BluetoothScreen.kt
+++ app/src/main/java/com/example/bletutorial/presentation/BluetoothScreen.kt
@@ -4,7 +4,6 @@ import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.location.Location
 import android.util.Log
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -35,7 +34,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.text.input.KeyboardType
@@ -219,7 +217,7 @@ fun BluetoothScreen(
                 var gpsLongitude by remember { mutableStateOf(0.0) }
                 var gpsAltitude by remember { mutableStateOf(0.0) }
 
-                val dataList = mutableListOf<DataInfo>()
+                var dataList = mutableListOf<DataInfo>()
 
                 Column(
                     modifier = Modifier.fillMaxSize(),
@@ -283,6 +281,7 @@ fun BluetoothScreen(
                             onClick = {
                                 isCollecting = false
                                 Log.d("test", "Recording data stopped")
+                                dataList = mutableListOf()
                             },
                             modifier = Modifier.fillMaxWidth(0.5f),
                             enabled = isCollecting // Enable the button only when collecting
@@ -385,7 +384,6 @@ fun BluetoothScreen(
                             DataInfoItem(dataInfo = dataInfo, onResendClick = { resendClick(dataInfo) })
                         }
                     }
-
                 }
 
             }else if(bleConnectionState == ConnectionState.Disconnected){
@@ -458,11 +456,13 @@ fun DataInfoItem(dataInfo: DataInfo, onResendClick: (DataInfo) -> Unit) {
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp)
-            .border(1.dp, Color.Gray)
-            .padding(8.dp)
+            .clickable {
+                // Optionally handle item click if needed
+            },
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Text(text = "Timestamp: ${dataInfo.performanceData?.performanceTime}")
-        Spacer(modifier = Modifier.height(8.dp))
 
         // Button to resend the API for this specific item
         Button(onClick = {onResendClick(dataInfo)}) {
